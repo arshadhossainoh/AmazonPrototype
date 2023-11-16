26789fa6b343b43531aeff4d8c35c21862ef7231
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-11-16 17:24:44 -0500

    delete products from html when clicking delete

--- javascript-amazon-project/scripts/checkout.js
+++ javascript-amazon-project/scripts/checkout.js
@@ -100,6 +100,10 @@ document.querySelectorAll(".js-delete-link").forEach((link) => {
   link.addEventListener("click", () => {
     const productId = link.dataset.productId;
     removeFromCart(productId);
-    console.log(cart);
+
+    const container = document.querySelector(
+      `.js-cart-item-container-${productId}`
+    );
+    container.remove();
   });
 });
