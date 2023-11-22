39e610fbf91142c164972992bbfc23a717a98980
arshadhossainoh <arshadhossain.oh@gmail.com>
2023-11-21 23:21:16 -0500

    worked on delivery deliveryOptions

--- index.js
+++ index.js
@@ -1,30 +1,52 @@
-// array = ['make dinner', 'wash dishes', 'watch youtube'];
+let stocks = {
+  Fruits: ["straberry", "grapes", "banana", "apple"],
+  liquid: ["water", "ice"],
+  holder: ["cone", "cup", "stick"],
+  toppings: ["chocolate", "peanuts"],
+};
 
-// array.forEach((value, index) => {
-//     if (value ==='wash dishes'){
-//         return;
-//     }
-//     console.log(index);
-//     console.log(value);
-// });
+let is_store_open = true;
 
+function time(ms) {
+  return new Promise((resolve, reject) => {
+    if (is_store_open) {
+      setTimeout(resolve, ms);
+    } else {
+      reject(console.log("store is closed now"));
+    }
+  });
+}
 
-//array = [1,-3,5]
-//array2 = array.filter((value, index)=>{
-    // if(value >=0){
-    //     return true;
-    // }else {
-    //     return false;
-    // }
-  //following line does same as previous line
-  //return value >= 0;
-//});
-//console.log(array2);
+async function kitchen() {
+  try {
+    await time(2000);
+    console.log(`${stocks.Fruits[0]} selected`);
 
-let array = [1,2,3,4]
+    await time(0000);
+    console.log("start the production");
 
-array.forEach((value, index)=>{
-    console.log(index);
-    console.log(value);
-})
+    await time(2000);
+    console.log("cut the fruit");
 
+    await time(1000);
+    console.log(`${stocks.liquid[0]} & ${stocks.liquid[1]} are added`);
+
+    await time(1000);
+    console.log("start the machine");
+
+    await time(2000);
+    console.log(`ice cream placed on ${stocks.holder[0]}`);
+
+    await time(3000);
+    console.log(`${stocks.toppings[0]} is selected`);
+
+    await time(2000);
+    console.log("serve ice cream");
+  } catch (error) {
+    console.log("customer left", error);
+  } finally {
+    console.log("day ended, store closed");
+  }
+}
+
+kitchen();
